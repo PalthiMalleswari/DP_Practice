4ccc52a9f52a9b4d55345b052e299c48b0aae232
PalthiMalleswari <palthimalleswari@gmail.com>
2026-06-08 19:13:56 +0530

    Update Ones and Zeroes.py

--- DP/0-1 Knapsack/Ones and Zeroes.py
+++ DP/0-1 Knapsack/Ones and Zeroes.py
@@ -1,4 +1,5 @@
 #Problem - https://leetcode.com/problems/ones-and-zeroes/
+# Refer Ones and Zeros.md File For Approach
 
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
@@ -24,4 +25,54 @@ class Solution:
         return get_max_len(le-1,m,n)
 
 #Time Complexity - O(Len(Strs)*M*N)
+#Space Complexity - O(len(strs)*m*n) + Recursion Call Stack Space
+
+#======================= 3D Bottom Up Approach ===========
+
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        
+        lenn = len(strs)
+        dp = [[[0]*(n+1) for _ in range(m+1)] for _ in range(lenn+1)]
+
+        for i in range(1,lenn+1):
+            
+            zeros = strs[i-1].count('0')
+            ones = strs[i-1].count('1')
+
+            for j in range(m+1):
+                for k in range(n+1):
+                    dp[i][j][k] = dp[i-1][j][k]
+
+                    if j>=zeros and k>=ones:
+                        dp[i][j][k] = max(dp[i][j][k],dp[i-1][j-zeros][k-ones]+1)
+
+        return dp[lenn][m][n]
+
+#Time Complexity - O(Len(Strs)*M*N)
 #Space Complexity - O(len(strs)*m*n)
+
+#=================== 2D Space Optimization Approach ==============
+
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        
+        lenn = len(strs)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        for st in strs:
+            
+            zeros = st.count('0')
+            ones = st.count('1')
+
+            for j in range(m,zeros-1,-1):
+                for k in range(n,ones-1,-1):
+   
+                        dp[j][k] = max(dp[j][k],dp[j-zeros][k-ones]+1)
+
+        return dp[m][n]
+
+Time Complexity - O(len(strs)*m*n)
+Space Complexity - O(m*n)
+
+
