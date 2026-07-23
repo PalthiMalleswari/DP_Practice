fad01c722f1bfd3f1ef6da9c4a87f68a08fdfea1
Palthi Malleswari <palthimalleswari@gmail.com>
2026-07-23 22:49:19 +0530

    Create Dungeon Game.py

---
+++ DP/DP on Grid/Dungeon Game.py
@@ -0,0 +1,36 @@
+#Problem - https://leetcode.com/problems/dungeon-game/description/
+
+#Refer Solution -  https://leetcode.com/problems/dungeon-game/solutions/745340/post-dedicated-to-beginners-of-dp-or-hav-whjg/
+
+#============== Memorization Approach =================
+
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        
+        m = len(dungeon)
+        n =  len(dungeon[0])
+        memo = {}
+
+        def func(r,c):
+            
+            if r>=m or c>=n:
+                return float('inf')
+
+            if r==m-1 and c == n-1:
+                return -dungeon[r][c]+1 if dungeon[r][c] <=0 else 1 
+            
+            if (r,c) in memo:
+                return memo[(r,c)]
+
+            down =func(r+1,c)
+            right = func(r,c+1)
+
+            min_req = min(down,right) - dungeon[r][c]
+            memo[(r,c)] = 1 if min_req <=0 else min_req
+            return memo[(r,c)]
+            
+        ans = func(0,0)
+        return ans
+
+Time Complexity - O(M*N)
+Space Complexity - O(M*N)+stack space
