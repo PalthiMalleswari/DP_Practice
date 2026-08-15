e318d5a3d908638341425e9156334b4c6f3e6da0
Palthi Malleswari <palthimalleswari@gmail.com>
2026-08-15 22:21:05 +0530

    Update Word Break.py

--- Recursion & BackTracking/Solutions/Word Break.py
+++ Recursion & BackTracking/Solutions/Word Break.py
@@ -35,4 +35,69 @@ class Solution:
         return segment_s(s)
 
 # Time Complexity - 
-# Space Complexity -  
+# Space Complexity -
+
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        n = len(s)
+        memo = {}
+
+        def can_segment(ind):
+            
+            if ind>=n:
+                return True
+            
+            if ind in memo:
+                return memo[ind]
+
+            for word in wordDict:
+                
+                wl = len(word)
+                if ind+wl-1 < n:
+                    
+                    i = 0
+                    
+                    while i<wl and s[ind+i]==word[i]:
+                        i+=1
+                    
+                    if i<wl:
+                        continue
+                    
+                    if can_segment(ind+i):
+                        memo[ind] = True
+                        return True
+
+            memo[ind] = False
+            return False
+
+        # return can_segment(0)
+
+        dp = [False]*(n+1)
+
+        dp[0] = True
+
+        for i in range(1,n+1):
+
+            for word in wordDict:
+
+                wl = len(word)
+
+                if i-wl>=0:
+
+                    st = i-wl
+                    j = 0
+                    
+                    while j<wl and s[st+j]==word[j]:
+                        j+=1
+                    
+                    if j<wl:
+                        continue
+                    # print(j,wl,word,i)
+                    dp[i] = dp[i] or dp[i-wl]
+        # print(dp)
+        return dp[n]
+
+
