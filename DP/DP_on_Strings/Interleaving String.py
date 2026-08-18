eb66b1bbcad73821621a2afbbf79d5869b048b72
Palthi Malleswari <palthimalleswari@gmail.com>
2026-08-18 09:42:20 +0530

    Update Interleaving String.py

--- DP/DP_on_Strings/Interleaving String.py
+++ DP/DP_on_Strings/Interleaving String.py
@@ -73,6 +73,30 @@ class Solution:
 Time Complexity  - O(M*N)
 Space Complexity - O(M*N)+Stack Space
 
+#============= Bottom up =============
+
+        dp = [[False]*(n2+1) for _ in range(n1+1)]
+        dp[0][0] = True
+
+        for i in range(1,n1+1):
+
+            dp[i][0] = dp[i-1][0] and s1[i-1]==s3[i-1]
+        
+        for j in range(1,n2+1):
+            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+
+        for i in range(1,n1+1):
+
+            for j in range(1,n2+1):
+
+                dp[i][j] = dp[i][j-1] and s2[j-1] == s3[i+j-1] or \
+                            dp[i-1][j] and s1[i-1] == s3[i+j-1]
+  
+        return dp[n1][n2]
+
+Time Complexity - O(M*N)
+Space Complexity - O(M*N)
+
 #=============== Space Optimization and Optimal Approach  =================
 
   dp = [False]*(n2+1)
